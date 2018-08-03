b30edfed75240eaea39b2e6d3876fb2a30946327
jesse <813070871@qq.com>
2018-08-03 13:52:17 +0800

    没有函数的

---
+++ Web-Monitor-1.sh
@@ -0,0 +1,14 @@
+#/bin/bash
+if [ $# -ne 1 ]
+  then
+    echo $"usage:$0 url"
+    exit 1
+fi
+wget --spider -q -o /dev/null --tries=1 -T 5 $1
+#-T为指定的超时时间
+if [ $? -eq 0 ]
+  then 
+    echo "$1 is yes"
+else
+    echo "$1 is no"
+fi
