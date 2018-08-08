d5f92c5404d7caf12572a514e89715d9bf9edbeb
jesssecat <813070871@qq.com>
2018-08-08 15:08:13 +0800

    生成是个按照要求的文件名

---
+++ rand-file-ten.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+Path=/jesse
+[ -d "$Path" ]||mkdir -p $Path
+for n in `seq 10`
+do
+    random=$(openssl rand -base64 40|sed 's#[^a-z]##g'|cut -c 2-11)
+    touch $Path/${random}_jesse.html
+done
