26e4565718ece7646ff7409bd8fb80de43b9e8ad
root <root@node2.com>
2018-09-21 11:02:28 +0800

    一键安装python3

---
+++ Python3-install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+wget https://www.python.org/ftp/python/3.6.3/Python-3.6.3.tgz
+
+tar -zxvf Python-3.6.3.tgz -C /usr/local/
+mv /usr/local/Python-3.6.3/ /usr/local/python3
+cd /usr/local/python3/Python-3.6.3
+yum install -y gcc zlib zlib-devel
+./configure -with-ssl
+echo "-----------------code------------------:"$?
+make && make install
+echo "-----------------code------------------:"$?
+which python
+which python3
+#a=`pwd`/Python-3.6.3.tgz
+#rm -rf $a
+python3
+exit()
